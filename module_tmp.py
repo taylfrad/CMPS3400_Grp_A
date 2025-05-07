97bea8915c5f03e5712b14ec4c010890e1127ba6
taylfrad <taylor.fradella@selu.edu>
2025-05-07 14:32:29 -0500

    added module_tmp.py

---
+++ module_tmp.py
@@ -0,0 +1,76 @@
+#Version: v0.1
+#Date Last Updated: 1-12-2025
+
+#DELETE THIS comment
+#  Follow the coding standards listed in coding_standards.pdf 
+#  Delete the sections in this template if not used
+
+#%% MODULE BEGINS
+module_name_gl = 'write the file name w/o file extension'
+
+'''
+Version: <***>
+
+Description:
+    <***>
+
+Authors:
+    <***>
+
+Date Created     :  <***>
+Date Last Updated:  <***>
+
+Doc:
+    <***>
+
+Notes:
+    <***>
+'''
+
+#%% IMPORTS                    ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+if __name__ == "__main__":
+   import os
+   #os.chdir("./../..")
+#
+
+#custom imports
+
+
+#other imports
+
+
+#%% USER INTERFACE              ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+
+#%% CONSTANTS                   ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+
+#%% CONFIGURATION               ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+
+
+#%% INITIALIZATIONS             ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+
+#%% DECLARATIONS                ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+#Global declarations Start Here
+
+
+
+#Class definitions Start Here
+
+
+
+#Function definitions Start Here
+def main():
+    pass
+#
+
+#%% SELF-RUN                   ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#Main Self-run block
+if __name__ == "__main__":
+    
+    print(f"\"{module_name_gl}\" module begins.")
+    
+    main()
